3766326afafc7022c8f9e8f996fab6041e387352
Jon Ogea <38058311+Fireyheart@users.noreply.github.com>
2023-03-25 10:44:24 -0500

    feat(cork-1): set up RTK and sample slice

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,5 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  env: {node: true},
 };
---
+++ src/index.js
@@ -0,0 +1,18 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from '../app.json';
+import {store} from './app/store';
+import {Provider} from 'react-redux';
+
+const StatefulApp = () => {
+  return (
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+};
+AppRegistry.registerComponent(appName, () => StatefulApp);
